378614e3291c0fc898a768d96c055e70af1164f3
Garrett <gcamoin5@gmail.com>
2023-06-22 17:19:48 -0400

    commit tree 7e84bceb73ec62feeb2f1a350d886c3e9feeee07

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
+import "./App.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
